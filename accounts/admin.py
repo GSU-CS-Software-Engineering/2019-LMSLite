892d492efe23ec7598bacca418895753827abded
chino <chino.s.ugwumadu@gmail.com>
2019-03-03 21:34:03 -0500

    added logout functionality and fist name and last name to user admin creation form

--- accounts/admin.py
+++ accounts/admin.py
@@ -27,7 +27,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')}
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name')}
         ),
     )
     search_fields = ('email',)
