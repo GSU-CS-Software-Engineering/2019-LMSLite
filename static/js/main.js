e0c75c42fa6f85f206d1824704e973d4d39d39f0
as12660 <as12660@georgiasouthern.edu>
2019-03-27 19:05:13 -0400

    Added profile info and slight css

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,36 @@ function navBarFunction() {
     x.className = "topnav";
   }
 }
+
+function showHideQuiz() {
+    var x = document.getElementById("quizForm");
+    x.style.display = "block";
+
+    var y = document.getElementById("courseDesc");
+    y.style.display = "none";
+
+    var z = document.getElementById("hmwkForm");
+    z.style.display = "none";
+}
+
+function showHideHmwk() {
+    var x = document.getElementById("quizForm");
+    x.style.display = "none";
+
+    var y = document.getElementById("courseDesc");
+    y.style.display = "none";
+
+    var z = document.getElementById("hmwkForm");
+    z.style.display = "block";
+}
+
+function showHideCourse() {
+    var x = document.getElementById("quizForm");
+    x.style.display = "none";
+
+    var y = document.getElementById("courseDesc");
+    y.style.display = "block";
+
+    var z = document.getElementById("hmwkForm");
+    z.style.display = "none";
+}
