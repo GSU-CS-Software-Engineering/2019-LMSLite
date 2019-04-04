d6aa25b574f0dcb6f61abea7d9de8665f180cce8
Noah Hickey <bh05319@georgiasouthern.edu>
2019-04-04 13:21:55 -0400

    framework/logic to create and remove questions on quiz page

--- static/js/main.js
+++ static/js/main.js
@@ -39,3 +39,36 @@ function showHideCourse() {
     var z = document.getElementById("hmwkForm");
     z.style.display = "none";
 }
+
+function removeElement(elementId){
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+
+
+}
+function addElement(parentId, elementTag, elementId, html) {
+    // Adds an element to the document
+    var p = document.getElementById(parentId);
+    var newElement = document.createElement(elementTag);
+    newElement.setAttribute('id', elementId);
+    newElement.innerHTML = html;
+    p.appendChild(newElement);
+}
+var qid= 12;
+
+function addQ(){
+var html= '<p> <select name="Question12type" id="id_Question12">' +
+    '<option value="3">MA</option>' +
+    '<option value="5" selected>TF</option>' +
+    '<option value="6">ESS</option> ' +
+    '<option value="4">FIB</option>' +
+    '<option value="2">SR</option>' +
+    '<option value="1">MC</option>' +
+    '</select>' +
+
+
+    '</p>'
+addElement('quizForm', 'p', 'id_Question12', html );
+
+
+}
