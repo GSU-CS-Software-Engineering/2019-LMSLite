a04f320e5c8843ef385124a23dabc890f0c73f92
Cameron  Morgan <cm09516@georgiasouthern.edu>
2019-04-05 00:43:40 -0400

    answer boxes in quiz edit form3

--- static/js/quizExtention.js
+++ static/js/quizExtention.js
@@ -1,72 +1,81 @@
-
-
-
 function newQuestion() {
-  event.preventDefault();
+    event.preventDefault();
 
-  var i = document.querySelectorAll("label[for*=id_Question]:not([for*=Answer])").length + 1;
+    var i = document.querySelectorAll("label[for*=id_Question]:not([for*=Answer])").length + 1;
 
-  var quizForm = document.getElementById("quizForm");
+    var quizForm = document.getElementById("quizForm");
 
-  var p1 = document.createElement("p");
-  p1.setAttribute("id", "pExtender"+i);
+    var p1 = document.createElement("p");
+    p1.setAttribute("id", "pExtender" + i);
 
-  var selection = document.createElement("select");
-  selection.setAttribute("id", "Question" +i+ "type");
-  selection.setAttribute("name", "Question" +i+ "type");
+    var selection = document.createElement("select");
+    selection.setAttribute("id", "Question" + i + "type");
+    selection.setAttribute("name", "Question" + i + "type");
 
-  var z1 = document.createElement("option");z1.setAttribute("value", "1");
-  var t1 = document.createTextNode("MC");z1.appendChild(t1);
+    var z1 = document.createElement("option");
+    z1.setAttribute("value", "1");
+    var t1 = document.createTextNode("MC");
+    z1.appendChild(t1);
 
-  var z2 = document.createElement("option");z2.setAttribute("value", "6");
-  var t2 = document.createTextNode("ESS");z2.appendChild(t2);
 
-  var z3 = document.createElement("option");z3.setAttribute("value", "3");
-  var t3 = document.createTextNode("MA");z3.appendChild(t3);
+    var z2 = document.createElement("option");
+    z2.setAttribute("value", "6");
+    var t2 = document.createTextNode("ESS");
+    z2.appendChild(t2);
 
-  var z4 = document.createElement("option");z4.setAttribute("value", "4");
-  var t4 = document.createTextNode("FIB");z4.appendChild(t4);
+    var z3 = document.createElement("option");
+    z3.setAttribute("value", "3");
+    var t3 = document.createTextNode("MA");
+    z3.appendChild(t3);
 
-  var z5 = document.createElement("option");z5.setAttribute("value", "5");
-  var t5 = document.createTextNode("TF");z5.appendChild(t5);
+    var z4 = document.createElement("option");
+    z4.setAttribute("value", "4");
+    var t4 = document.createTextNode("FIB");
+    z4.appendChild(t4);
 
-  var z6 = document.createElement("option");z6.setAttribute("value", "2");
-  var t6 = document.createTextNode("SR");z6.appendChild(t6);
+    var z5 = document.createElement("option");
+    z5.setAttribute("value", "5");
+    var t5 = document.createTextNode("TF");
+    z5.appendChild(t5);
 
-  var z7 = document.createElement("option");z7.setAttribute("value", "7");
-  var t7 = document.createTextNode("");z7.appendChild(t7);
+    var z6 = document.createElement("option");
+    z6.setAttribute("value", "2");
+    var t6 = document.createTextNode("SR");
+    z6.appendChild(t6);
 
-  quizForm.appendChild(p1);
-  p1.appendChild(selection);
-  selection.appendChild(z7);selection.appendChild(z1);selection.appendChild(z2);
-  selection.appendChild(z3);selection.appendChild(z4);selection.appendChild(z5);
-  selection.appendChild(z6);
+    var z7 = document.createElement("option");
+    z7.setAttribute("value", "7");
+    var t7 = document.createTextNode("");
+    z7.appendChild(t7);
 
-  var p = document.createElement("p");
-  p.setAttribute("id", "paragraphExtender"+i);
+    quizForm.appendChild(p1);
+    p1.appendChild(selection);
+    selection.appendChild(z7);
+    selection.appendChild(z1);
+    selection.appendChild(z2);
+    selection.appendChild(z3);
+    selection.appendChild(z4);
+    selection.appendChild(z5);
+    selection.appendChild(z6);
 
-  var qlabel = document.createElement("label");
-  qlabel.setAttribute("for", "id_Question"+i);
+    var p = document.createElement("p");
+    p.setAttribute("id", "paragraphExtender" + i);
 
-    var alabel = document.createElement("label");
-    alabel.setAttribute("for", "id_QuestionAnswer"+i);
+    var qlabel = document.createElement("label");
+    qlabel.setAttribute("for", "id_Question" + i);
 
-    var t = document.createTextNode("Question "+i+ " ");
+    var t = document.createTextNode("Question " + i + " ");
     qlabel.appendChild(t);
-    alabel.appendChild(t);
 
-  var sQ = document.getElementById("Question"+i+"type").selectedIndex;
-  var sName = document.getElementById("Question"+i+"type").options;
+    var sQ = document.getElementById("Question" + i + "type").selectedIndex;
+    var sName = document.getElementById("Question" + i + "type").options;
 
-  var qtArea = document.createElement("TextArea");
-  qtArea.setAttribute("for", "id_Question"+i);
-  qtArea.setAttribute("placeholder", sName[sQ].text +" Question Here");
-
-    var atArea = document.createElement("TextArea");
-    atArea.setAttribute("for", "id_QuestionAnswer"+i);
+    var qtArea = document.createElement("TextArea");
+    qtArea.setAttribute("for", "id_Question" + i);
+    qtArea.setAttribute("placeholder", sName[sQ].text + " Question Here");
 
     qtArea.setAttribute("cols", "40");
-    qtArea.setAttribute("rows" , "1");
+    qtArea.setAttribute("rows", "1");
     qtArea.setAttribute("style", "height: 5rem");
     qtArea.setAttribute("maxlength", "1000");
 
@@ -74,13 +83,34 @@ function newQuestion() {
     quizForm.appendChild(p);
     p.appendChild(qlabel);
     p.appendChild(qtArea);
-    p.appendChild(alabel);
-    p.appendChild(atArea);
 
-    var showAns=document.getElementById("qSec");
-    document.getElementById("demo").innerHTML= showAns.innerHTML;
 
-    i++;
+}
+
+function newAnswer() {
+    event.preventDefault();
+
+    var x = document.querySelectorAll("label[for*=id_QuestionAnswer]").length + 1;
+
+    var quizForm = document.getElementById("quizForm");
+
+    var p2 = document.createElement("p2");
+    p2.setAttribute("id", "paExtend" + x);
+
+    var alabel = document.createElement("label");
+    alabel.setAttribute("for", "id_QuestionAnswer" + x);
+
+    var t2 = document.createTextNode("Answer" + x + " ");
+    alabel.appendChild(t2);
+
+    var atArea = document.createElement("TextArea");
+    atArea.setAttribute("for", "id_QuestionAnswer" + x);
+
+    quizForm.appendChild(p2);
+    p2.appendChild(alabel);
+    p2.appendChild(atArea);
+
+
 
 
 }
