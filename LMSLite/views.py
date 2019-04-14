6b598dd834dbe2faf7e57489585085909ba483e7
chino <chino.s.ugwumadu@gmail.com>
2019-04-14 10:29:38 -0400

    Finished download link, added urls to templates

--- LMSLite/views.py
+++ LMSLite/views.py
@@ -1,17 +1,37 @@
+import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.encoding import smart_str
 
-from courses.models import Homework
+from accounts.models import Professor
+from courses.models import Assignment
 
 
 def index(request):
+    context_dict = {}
 
-    return render(request, 'index.html')
+    if request.user.is_authenticated and request.user.role == 1:
+        d = datetime.datetime.today()
+        prof = Professor.objects.get(id=request.user.id)
+        courses = prof.courses.all()
+        quizzes = []
+
+        x = 0
+        for course in courses:
+            for quiz in course.quizes.all():
+                if quiz.due_date.replace(tzinfo=None) > d and x < 5:
+                    quizzes.append(quiz)
+                    x+=1
+
+        context_dict['quizzes'] = quizzes
+        context_dict['courses'] = courses
+
+    return render(request, 'index.html', context_dict)
 
 
 def download(request, id):
     response = HttpResponse(content_type='application/force-download')
-    response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(Homework.objects.get(id=id))
+    response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(Assignment.objects.get(id=id))
 
     return response
